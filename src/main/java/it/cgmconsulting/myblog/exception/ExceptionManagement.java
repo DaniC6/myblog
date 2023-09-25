74d9bc40ccae804768ec36ccd82c82ac34a26810
Daniele Caramanica <danielecaramanica@gmail.com>
2023-09-25 16:06:40 +0200

    WRITE ExceptionMnagement and ResourceNotFoundException

--- src/main/java/it/cgmconsulting/myblog/exception/ExceptionManagement.java
+++ src/main/java/it/cgmconsulting/myblog/exception/ExceptionManagement.java
@@ -1,10 +1,17 @@
 package it.cgmconsulting.myblog.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 @ControllerAdvice
 public class ExceptionManagement {
 
-
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class})
+    public ResponseEntity<?> methodArgumentTypeMismatchException (MethodArgumentTypeMismatchException ex) {
+       return new ResponseEntity<String> ( ex.getMessage (), HttpStatus.BAD_REQUEST );
+    }
 
 }
